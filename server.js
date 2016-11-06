72763f5e433cac0a6b1af3dcec440ab8d60de7ca
Arshad Khan <arshdkhn1@gmail.com>
2016-11-06 19:45:49 +0000

    adding new stuff

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 var express = require("express")
+    , crypto = require("./crypto")
     , port = process.env.PORT || 8080
-    , path = require("path");
+    , path = require("path"); 
 
 var app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
+
+
+app.get("/shorten/:url", function(req, res) {
+    var url = req.params.url;
+    res.send(crypto.encode(1000));
+});
+
+app.get("/:id", function(req, res) {
+    var id = req.params.id;
+    var redirectURL = crypto.decode(id).toString();
+    res.send(redirectURL);
+});
 
 app.listen(port, function() {
    console.log("App running on", port); 
