47059635ad2778952b31535ae8765f73d8b5905d
Arshad Khan <arshdkhn1@gmail.com>
2016-11-06 00:24:12 +0000

    adding changes

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-var express = require("express");
+var express = require("express")
+    , port = process.env.PORT || 8080
+    , path = require("path");
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(port, function() {
+   console.log("App running on", port); 
+});
