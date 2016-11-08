7fe197a9e60e246deb439a14d2ad229e5a371131
Arshad Khan <arshdkhn1@gmail.com>
2016-11-08 00:00:37 +0000

    updates

--- routes.js
+++ routes.js
@@ -2,10 +2,31 @@ var express = require("express");
 var router = express.Router();
 var crypto = require("./crypto");
 
-var styles = "<style>@import url('https://fonts.googleapis.com/css?family=Bungee+Hairline');" +
+var styles = "<style>@import url('https://fonts.googleapis.com/css?family=Cormorant+Garamond');" +
             "body{background: #fefefe; word-wrap: break-word;}" +
-            "p {font-size: 40px;color: #b33c66;font-family: 'Bungee Hairline', monospace;text-align: center;" +
-            "margin-top: 40vh;font-weight: 900;word-spacing: 2px;}</style>";
+            "p {font-size: 30px;color: #b33c66;font-family: 'Cormorant Garamond', monospace;text-align: center;" +
+            "margin-top: 40vh;font-weight: 500;word-spacing: 2px;}</style>";
+            
+function verifyUrl(req, res, next) {
+    console.log("/shorten middleware called");
+    req.params.url +=  req.params[0];
+    console.log(req.params.url);
+    if (validateUrl(req.params.url)) {
+        req.db.collection("counter")
+        .find({_id: "counter"})
+        .toArray(function (err, docs) {
+            if (err) console.error("Error occurred while getting COUNTER document:", err);
+            
+            req.encodedId = crypto.encode(docs[0].count);
+            
+            next();
+        });
+    }
+    else {
+        var elem = "<p>Please enter correct and formatted url!</p>";
+        res.send(styles + elem);
+    }
+}
 
 
 function incrementCounter(req, res, next) {
@@ -45,20 +66,15 @@ function validateUrl(url) {
   return (format.test(url));
 }
 
-router.get("/", function(req, res) {
-    console.log("/ middleware called");
-   res.sendFile(require("path").join(__dirname, "public", "index.html")); 
-});
-
-// this is calling many times
-router.get("/:id", function(req, res) {
-    console.log("/:id middleware called");
-    var id = req.params.id;
+router.get("/:code", function(req, res) {
+    console.log("/:code middleware called with url", req.params.code);
+    var code = req.params.code.toString();
     // searching short-url-id
     req.db.collection("urls")
-        .find({_id: id})
+        .find({_id: code})
         .toArray(function(err, docs) {
             if(err) console.error("Error occurred while searching urls:", err);
+            console.log(docs);
             if(docs.length > 0)
                 res.redirect(docs[0]["original-url"]);
             else {
@@ -68,24 +84,7 @@ router.get("/:id", function(req, res) {
         });
 });
 
-router.get("/shorten/(http:\/\/|https:\/\/)[a-z0-9\-]+[.]\w+/", function(req, res, next) {
-    console.log("/shorten middleware called");
-    console.log(req.params);
-    if (validateUrl(req.params[0])) {
-        req.db.collection("counter")
-        .find({_id: "counter"})
-        .toArray(function (err, docs) {
-            if (err) console.error("Error occurred while getting COUNTER document:", err);
-            
-            req.encodedId = crypto.encode(docs[0].count);
-            
-            next();
-        });
-    }
-    else {
-        var elem = "<p>Please enter correct and formatted url!</p>";
-        res.send(styles + elem);
-    }
-}, [incrementCounter, insertUrlDocument, sendResponse]);
+// better solution needed 
+router.get("/shorten/:url*", [verifyUrl, incrementCounter, insertUrlDocument, sendResponse]);
 
 module.exports = router;
